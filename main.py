e98baf30e2cc7e39abcf2c1ab467f3f03e7b6ded
tomlafferty <tsl5046@gmail.com>
2023-03-01 23:09:05 -0500

    slot random generation

--- main.py
+++ main.py
@@ -6,6 +6,44 @@ MAX_LINES = 5
 MAX_DEP = 100
 MAX_BET = 50
 
+ROWS = 5
+COLS = 3
+
+symbol_count = {
+    'A': 2,
+    'B': 4,
+    'C': 6,
+    'D': 8,
+}
+
+def get_slot_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        
+        columns.append(column)
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row])
+
+
 def deposit():
     while True:
         amount = input("How much would you like to deposit? $")
@@ -57,5 +95,9 @@ def main():
         else:
             break
     print(f"You are all set for ${bet} on {lines} lines. All in for ${total_bet}. Good Luck!")
+
+    slots = get_slot_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+
 
 main()
